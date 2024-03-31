d1e1c2cd6f2c1c849539619d372868922ed68580
AlexAgruso <agrusoalexander@gmail.com>
2024-03-31 15:46:06 -0500

    fish config and global git config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# dotfiles
+alias dots="git --git-dir={$HOME}/.dotfiles --work-tree={$HOME}"
+
+# fuzzy finder
+fzf --fish | source
+
+# zoxide
+zoxide init fish | source
+alias cd="z"
--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,4 +1,4 @@
-function dots --wraps='git --work-tree=/home/alexander --git-dir=/home/alexander/.dotfiles' --description 'alias dots=git --work-tree=/home/alexander --git-dir=/home/alexander/.dotfiles'
+function dots --wraps='git --work-tree=${HOME} --git-dir=${HOME}.dotfiles' --description 'alias dots=git --work-tree=/home/alexander --git-dir=/home/alexander/.dotfiles'
   git --work-tree=/home/alexander --git-dir=/home/alexander/.dotfiles $argv
         
 end
