3e265f5d1a0c721c4b52eff0a698799e502f77b0
AlexAgruso <agrusoalexander@gmail.com>
2024-04-20 18:30:00 -0500

    update fish and git configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-alias g="git"
 
 # dotfiles
 alias dots="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
@@ -8,6 +5,43 @@ alias dots="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 # fuzzy finder
 fzf --fish | source
 
+# git
+alias g="git"
+
+# ls
+alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+
 # zoxide
 zoxide init fish | source
 alias cd="z"
+
+# miscellaneous
+alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
+alias update='sudo pacman -Syu'
+
+# Get fastest mirrors
+alias mirror="sudo cachyos-rate-mirrors"
+
